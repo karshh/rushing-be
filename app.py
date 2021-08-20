c7cf0c502ebe3bc2a8122def5e4b7c8d65d87563
karsh <usharma_@live.com>
2021-08-20 01:50:09 -0600

    Added flask-cors.

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, Blueprint
 from flask.json import jsonify
 from settings import MONGO_URL
 from database.db import initialize_db
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Initialize DB
 app.config['MONGODB_SETTINGS'] = {
