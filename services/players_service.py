173a6c8154833c708a2077fdf9787a4e45f722e4
karsh <usharma_@live.com>
2021-08-25 22:18:35 -0600

    Split up Lng to Lng and T for easier sort.

--- services/players_service.py
+++ services/players_service.py
@@ -4,28 +4,48 @@ from database.player import Player
 
 class PlayerService:
 
+    GET_PLAYERS_PROJECTION = { 
+        '_id': 0,
+        'Lng': {
+            '$cond': [ 
+                { '$eq': ['$LngT', True ] },
+                { '$concat': [ {'$toString': '$Lng'}, 'T' ]}, 
+                {'$toString': '$Lng'} 
+            ]
+        },
+        'Player': 1,
+        'Team': 1,
+        'Pos': 1,
+        'Att': 1,
+        'Att/G': 1,
+        'Yds': 1,
+        'Avg': 1,
+        'Yds/G': 1,
+        'TD': 1,
+        '1st': 1,
+        '1st%': 1,
+        '20+': 1,
+        '40+': 1,
+        'FUM': 1
+    }
+    
     def get_players(self, filterName, sortColumn, sortDirection, skip, limit):
-        pipeline = [{ "$project": { "_id": 0 }}]
-
+        pipeline = []
         player_objects = Player.objects(playerName__icontains=filterName)
         size = player_objects.count()
         
-        if sortColumn != 'Lng':
-            pipeline.append({ "$sort": { sortColumn: int(sortDirection) }})
-            if skip:
-                pipeline.append({ "$skip": int(skip) })
-            if limit:
-                pipeline.append({ "$limit": int(limit) })
-
+        sortAggregate = { sortColumn: int(sortDirection) }
+        if sortColumn == 'Lng':
+            sortAggregate['LngT'] = int(sortDirection)
+        pipeline.append({ '$sort': sortAggregate})
+        pipeline.append({ '$project': self.GET_PLAYERS_PROJECTION })
+        if skip:
+            pipeline.append({ '$skip': int(skip) })
+        if limit:
+            pipeline.append({ '$limit': int(limit) })
+        
         player_cursor = player_objects.aggregate(pipeline)
         players = loads(dumps(player_cursor))
-        if sortColumn == 'Lng':
-            players.sort(
-                key=lambda x: (int(str(x['Lng']).replace('T', '')), str(x['Lng']).find('T')),
-                reverse=int(sortDirection) < 0
-            )
-            if skip and limit:
-                players = players[int(skip):int(skip)+int(limit)]
         return {
             'size': size,
             'players': players
@@ -45,7 +65,8 @@ class PlayerService:
                 rushingAvg = new_player.get('Avg'),
                 rushingYdsG = new_player.get('Yds/G'),
                 rushingTouchdowns = new_player.get('TD'),
-                rushingLongest = str(new_player.get('Lng')),
+                rushingLongest = int(str(new_player.get('Lng')).replace('T', '')),
+                rushingLongestT = str(new_player.get('Lng')).find('T') >= 0,
                 rushingFD = new_player.get('1st'),
                 rushingFDP = new_player.get('1st%'),
                 rushing20plus = new_player.get('20+'),
@@ -55,7 +76,7 @@ class PlayerService:
             player.validate()
             players.append(player)
         current_players.delete()
-        Player.objects.insert(players)
+        if players: Player.objects.insert(players)
 
     @staticmethod
     def _convert_to_int(value):
